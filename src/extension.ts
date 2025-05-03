0871b19635a1512dbc7401228f1d7785d0b61bcb
Daniel Ward <02danwar.buss@gmail.com>
2025-05-03 14:38:05 -0500

    sm bug fix

--- src/extension.ts
+++ src/extension.ts
@@ -297,13 +297,9 @@ export async function activate(context: vscode.ExtensionContext) {
 				}
 			);
 
-			welcomePanel.onDidDispose(
-				() => {
-					welcomePanel = undefined;
-				},
-				null,
-				context.subscriptions
-			);
+			welcomePanel.onDidDispose(() => {
+				welcomePanel = undefined;
+			}, null);
 
 			const welcomeFilePath = vscode.Uri.joinPath(
 				context.extensionUri,
